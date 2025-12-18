195bcb94dcd5ff39d0a519dfaa6c528ccef7a93d
bala-amypo <bala@amypo.in>
2025-12-18 18:13:04 +0000

    User commit

--- src/main/java/com/example/demo/service/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/serviceimpl/UserServiceImpl.java
@@ -1,29 +1,9 @@
-package com.example.service.impl;
+package com.example.service;
 
-import org.springframework.stereotype.Service;
+import com.example.model.User;
+import java.util.Optional;
 
-import com.example.demo.model.exception.NotFoundException;
-import com.example.demo.model.User;
-import com.example.demo.model.repository.UserRepository;
-import com.example.demo.model.service.UserService;
-
-@Service
-public class UserServiceImpl implements UserService {
-
-    private final UserRepository userRepository;
-
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public User register(User user) {
-        return userRepository.save(user);
-    }
-
-    @Override
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new NotFoundException("User not found"));
-    }
+public interface UserService {
+    User register(User user);
+    Optional<User> findByEmail(String email);
 }
