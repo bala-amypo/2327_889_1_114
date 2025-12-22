6cf1e0f391ad2b911e413df2cb83df3035405a07
bala-amypo <bala@amypo.in>
2025-12-22 13:35:26 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -8,198 +8,21 @@ public class AuthRequest {
     public AuthRequest() {
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setEmail(String email) {
+    public AuthRequest(String email, String password) {
         this.email = email;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
-}
-package com.example.demo.dto;
-
-public class AuthRequest {
-
-    private String email;
-    private String password;
-
-    public AuthRequest() {
-    }
 
     public String getEmail() {
         return email;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
-package com.example.demo.dto;
-
-public class AuthRequest {
-
-    private String email;
-    private String password;
-
-    public AuthRequest() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
-package com.example.demo.dto;
-
-public class AuthRequest {
-
-    private String email;
-    private String password;
-
-    public AuthRequest() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
-package com.example.demo.dto;
-
-public class AuthRequest {
-
-    private String email;
-    private String password;
-
-    public AuthRequest() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
-package com.example.demo.dto;
-
-public class AuthRequest {
-
-    private String email;
-    private String password;
-
-    public AuthRequest() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
-package com.example.demo.dto;
-
-public class AuthRequest {
-
-    private String email;
-    private String password;
-
-    public AuthRequest() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
-package com.example.demo.dto;
-
-public class AuthRequest {
-
-    private String email;
-    private String password;
-
-    public AuthRequest() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public void setPassword(String password) {
--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -17,7 +17,7 @@ public class RegisterRequest {
     public String getEmail() {
         return email;
     }
-    
+
     public String getPassword() {
         return password;
     }
