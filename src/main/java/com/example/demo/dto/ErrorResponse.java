7b8be371237d319c7d7a84677e60e6eb640ef3ac
bala-amypo <bala@amypo.in>
2025-12-23 08:55:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ErrorResponse.java
@@ -0,0 +1,20 @@
+   package com.example.demo.dto;
+    public static class ErrorResponse {
+        private int status;
+        private String message;
+        private LocalDateTime timestamp;
+        
+        public ErrorResponse(int status, String message, LocalDateTime timestamp) {
+            this.status = status;
+            this.message = message;
+            this.timestamp = timestamp;
+        }
+        
+        public int getStatus() { return status; }
+        public void setStatus(int status) { this.status = status; }
+        public String getMessage() { return message; }
+        public void setMessage(String message) { this.message = message; }
+        public LocalDateTime getTimestamp() { return timestamp; }
+        public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+    }
+}
