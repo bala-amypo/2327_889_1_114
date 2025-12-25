129cdf0a67f5f5b1c95db14b43b45605342ff7de
bala-amypo <bala@amypo.in>
2025-12-25 09:23:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ServiceCounterRequestDto.java
@@ -0,0 +1,32 @@
+package com.example.demo.dto;
+
+public class ServiceCounterDto {
+
+    private String counterName;
+    private String department;
+    private Boolean isActive;
+
+    public String getCounterName() {
+        return counterName;
+    }
+
+    public void setCounterName(String counterName) {
+        this.counterName = counterName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+    
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+    
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+}
