d96d6ae4d465c0c42006ea3c6a2333327f484a9b
bala-amypo <bala@amypo.in>
2025-12-25 08:14:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/ServiceCounterRepository.java
+++ src/main/java/com/example/demo/repository/ServiceCounterRepository.java
@@ -2,12 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.ServiceCounter;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface ServiceCounterRepository extends JpaRepository<ServiceCounter, Long> {
-    boolean existsByName(String name);
     List<ServiceCounter> findByIsActiveTrue();
 }
