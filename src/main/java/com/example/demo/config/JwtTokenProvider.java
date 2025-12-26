13fbff342557a37de5dca616dcad2141f0b7bc6a
bala-amypo <bala@amypo.in>
2025-12-26 04:31:18 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtTokenProvider.java
+++ src/main/java/com/example/demo/config/JwtTokenProvider.java
@@ -1,5 +1,55 @@
 // package com.example.demo.config;
 
+// import io.jsonwebtoken.*;
+// import java.util.Date;
+// import java.util.HashMap;
+// import java.util.Map;
+
+// public class JwtTokenProvider {
+
+//     private final String secretKey;
+//     private final long validityInMs;
+
+//     public JwtTokenProvider(String secretKey, long validityInMs) {
+//         this.secretKey = secretKey;
+//         this.validityInMs = validityInMs;
+//     }
+
+//     public String generateToken(Long userId, String email, String role) {
+
+//         Map<String, Object> claims = new HashMap<>();
+//         claims.put("email", email);
+//         claims.put("role", role);
+
+//         return Jwts.builder()
+//                 // .setClaims(claims)
+//                 .setSubject(String.valueOf(userId))
+//                 .setIssuedAt(new Date())
+//                 .setExpiration(new Date(System.currentTimeMillis() + validityInMs))
+//                 .signWith(SignatureAlgorithm.HS256, secretKey)
+//                 .compact();
+//     }
+
+//     public boolean validateToken(String token) {
+//         try {
+//             Jwts.parser()
+//                     .setSigningKey(secretKey)
+//                     .parseClaimsJws(token);
+//             return true;
+//         } catch (Exception e) {
+//             return false;
+//         }
+//     }
+
+//     public Claims getClaims(String token) {
+//         return Jwts.parser()
+//                 .setSigningKey(secretKey)
+//                 .parseClaimsJws(token)
+//                 .getBody();
+//     }
+// }
+package com.example.demo.config;
+
 import com.example.demo.entity.User;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
