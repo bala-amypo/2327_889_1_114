71ce17b441bf9fb9d7b5423d9a1489ccbf14d2a1
bala-amypo <bala@amypo.in>
2025-12-26 13:21:25 +0000

    User commit

--- src/main/java/com/example/demo/service/TokenIssueService.java
+++ src/main/java/com/example/demo/service/TokenIssueService.java
@@ -5,10 +5,3 @@ import com.example.demo.entity.Token;
 public interface TokenIssueService {
     Token issue(Token token);
 }
-package com.example.demo.service;
-
-import com.example.demo.entity.Token;
-
-public interface TokenIssueService {
-    Token issue(Token token);
-}
