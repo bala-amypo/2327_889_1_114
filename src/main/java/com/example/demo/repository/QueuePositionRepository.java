12b6e11d5c87d746951ffcbabec8f4010af5cc73
bala-amypo <bala@amypo.in>
2025-12-26 06:43:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/QueuePositionRepository.java
+++ src/main/java/com/example/demo/repository/QueuePositionRepository.java
@@ -1,13 +1,23 @@
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.*;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import java.util.List;
+// import java.util.Optional;
+
+// public interface QueuePositionRepository extends JpaRepository<QueuePosition, Long> {
+//     Optional<QueuePosition> findByToken_Id(Long tokenId);
+// }
+
+
 package com.example.demo.repository;
 
-import com.example.demo.entity.*;
+import com.example.demo.entity.QueuePosition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface QueuePositionRepository extends JpaRepository<QueuePosition, Long> {
-    Optional<QueuePosition> findByToken_Id(Long tokenId);
+@Repository
+public interface QueueRepository extends JpaRepository<QueuePosition, Long> {
+    // You can add custom queries here if needed
 }
-
-
