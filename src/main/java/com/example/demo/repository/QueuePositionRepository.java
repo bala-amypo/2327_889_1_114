a2a79f2b8b8650092940b8401c2375d64b9ae139
bala-amypo <bala@amypo.in>
2025-12-28 05:28:10 +0000

    User commit

--- src/main/java/com/example/demo/repository/QueuePositionRepository.java
+++ src/main/java/com/example/demo/repository/QueuePositionRepository.java
@@ -2,10 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.QueuePosition;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-@Repository
 public interface QueuePositionRepository extends JpaRepository<QueuePosition, Long> {
     Optional<QueuePosition> findByToken_Id(Long tokenId);
 }
--- src/main/java/com/example/demo/repository/ServiceCounterRepository.java
+++ src/main/java/com/example/demo/repository/ServiceCounterRepository.java
@@ -1,11 +1,10 @@
+
 package com.example.demo.repository;
 
 import com.example.demo.entity.ServiceCounter;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
 public interface ServiceCounterRepository extends JpaRepository<ServiceCounter, Long> {
     List<ServiceCounter> findByIsActiveTrue();
 }
